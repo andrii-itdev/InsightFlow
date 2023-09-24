5ede285ee93721a03658a37b276d5d3a0d15d219
andrii-itdev <andrii.itdev@gmail.com>
2023-09-24 21:51:19 +0300

    #1 Added initial scaffolding of the TaskManagerController

---
+++ src/InsightFlow/ProcessHub/Controllers/TaskManagerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProcessHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskManagerController : ControllerBase
+    {
+
+        // POST api/<TaskManagerController>
+        [HttpPost]
+        public void InitiateTask([FromBody] string name)
+        {
+        }
+
+        // POST api/<TaskManagerController>/TaskHash
+        [HttpPost("{taskHash}")]
+        public void CancelTask(int taskHash)
+        {
+        }
+    }
+}
