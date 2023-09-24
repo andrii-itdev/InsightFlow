26946496259fbb6ba7d93674f4072838920f5b94
andrii-itdev <andrii.itdev@gmail.com>
2023-09-24 21:24:24 +0300

    #1 Added initial scaffolding of the MonitorController

---
+++ src/InsightFlow/ProcessHub/Controllers/MonitorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonitorController : ControllerBase
+    {
+        // GET: api/<MonitorController>
+        [HttpGet]
+        public string Get()
+        {
+            return nameof(MonitorController);
+        }
+
+        // GET api/<MonitorController>/tasks
+        [HttpGet("Tasks")]
+        public IEnumerable<string> GetTasks()
+        {
+            return new string[] { };
+        }
+
+        // GET: api/<MonitorController>/TaskHash/Details
+        [HttpGet("Task/{taskHash}/Details")]
+        public string GetDetails(int taskHash)
+        {
+            return $"Details For {taskHash}";
+        }
+
+        // GET: api/<MonitorController>/Status/TaskHash
+        [HttpGet("Task/{taskHash}/Status")]
+        public string GetStatus(int taskHash)
+        {
+            return $"Status For {taskHash}";
+        }
+    }
+}
