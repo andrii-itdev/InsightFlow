30c99fbc589bfeec1c55ca4c33ed16c2fe9505f6
andrii-itdev <andrii.itdev@gmail.com>
2024-01-21 20:20:53 +0100

    #13 improved CoreDefinitions

--- src/Services/InsightFlow/Infrastructure/CoreDefinitions.ts
+++ src/Services/InsightFlow/Infrastructure/CoreDefinitions.ts
@@ -1,4 +1,9 @@
 ﻿
+// Basic types
+
+type Id = number;
+type RecordData = string | number;
+
 // Models
 type DataSet = {};
 type DataRecord = {};
@@ -7,6 +12,17 @@ type Permission = {};
 type User = {};
 type Watcher = {};
 type AlertNotification = {};
+
+type DataEntity = DataRecord | DataSet;
+type DataType = {};
+
+
+type ReadPermission = {};
+type CreatePermission = {};
+type UpdatePermission = {};
+type DeletePermission = {};
+
+type PermissionType = ReadPermission | CreatePermission | UpdatePermission | DeletePermission ;
 
 // Events
 type ProcessCreated = {};
@@ -35,8 +51,33 @@ type UserAuthenticated = {};
 type UserDeleted = {};
 
 type DataSetPermissionsChanged = {};
+type DataRecordPermissionsChanged = {};
 
 // Commands
+
+type CreateProcess = { processName : string };
+type StartProcess = { processId : Id };
+type CancelProcess = { processId : Id };
+
+type CreateDataSet = { dataSetName : string };
+type DeleteDataSet = { dataSetId : Id };
+type AddRecordDataSet = { dataSetId : Id, recordId : Id };
+type RemoveRecordDataSet = { dataSetId : Id, recordId : Id };
+
+type ShareDataSet = { dataSetId : Id, userId : Id };
+
+type CreateRecord = { recordData : RecordData, dataType : DataType };
+type UpdateRecord = { recordId : Id, recordData : RecordData };
+type DeleteRecord = { record : Id };
+
+type CreateRelationship = { first : DataEntity, second : DataEntity, name : string };
+type RemoveRelationship = { relationshipId : Id };
+
+type SetupWatcher = { watcherName : string, configuration : string };
+type SwitchWatcher = { watcherId : Id, flag : boolean };
+
+type GrantPermission = { userId : Id, what : DataEntity, permissionType : PermissionType};
+type RemovePermission = { userId : Id, what : DataEntity, permissionType : PermissionType};
 
 // Errors
 
@@ -67,10 +108,11 @@ type DomainEvent =
     UserAuthenticated |
     UserDeleted |
 
-    DataSetPermissionsChanged
+    DataSetPermissionsChanged |
+    DataRecordPermissionsChanged
     ;
 
 
 
-type Error = {};
+type ErrorType = {};
 
