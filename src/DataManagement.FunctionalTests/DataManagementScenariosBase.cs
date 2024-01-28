5d8ab9380faa4d2c8650aa6102e8fd6e466faab1
andrii-itdev <andrii.itdev@gmail.com>
2024-01-28 20:24:42 +0100

    #17 - Added DataManagement.FunctionalTests basic infrastructure

---
+++ src/DataManagement.FunctionalTests/DataManagementScenariosBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace DataManagement.FunctionalTests
+{
+    internal class DataManagementApplication //: WebApplicationFactory<Program>
+    {
+
+    }
+
+    public class DataManagementScenariosBase
+    {
+    }
+}
---
+++ src/DataManagement.FunctionalTests/DataSetControllerScenarios.cs
@@ -0,0 +1,35 @@
+namespace DataManagement.FunctionalTests
+{
+    public class DataSetControllerScenarios : DataManagementScenariosBase
+    {
+        [Fact]
+        public void GetAll_ReturnsAllDataSets()
+        {
+
+        }
+
+        [Fact]
+        public void GetById_ReturnsDataSetWithSuccessCode()
+        {
+
+        }
+
+        [Fact]
+        public void CreateDataSet_ReturnsSuccessfulCode()
+        {
+
+        }
+
+        [Fact]
+        public void UpdateDataSet_ReturnsSuccessfulCode()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteDataSet_ReturnsSuccessfulCode()
+        {
+
+        }
+    }
+}
