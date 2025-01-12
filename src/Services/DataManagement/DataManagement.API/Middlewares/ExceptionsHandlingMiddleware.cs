e68671a9e8fc3276cd18d016fb473f4dda049fe6
andrii-itdev <andrii.itdev@gmail.com>
2025-01-12 20:05:29 +0100

    #26 minor improvement

--- src/Services/DataManagement/DataManagement.API/Middlewares/ExceptionsHandlingMiddleware.cs
+++ src/Services/DataManagement/DataManagement.API/Middlewares/ExceptionsHandlingMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿namespace DataManagement.API.Middlewares
 {
+    using Microsoft.AspNetCore.WebUtilities;
     using Microsoft.Net.Http.Headers;
     using Serilog;
     using System.Net.Mime;
@@ -26,15 +27,20 @@
                 _logger.Error(ex, $"{nameof(HttpClient)} request failed.", nameof(ExceptionsHandlingMiddleware));
                 context.Response.Headers[HeaderNames.ContentType] = MediaTypeNames.Application.Json;
                 context.Response.StatusCode = (int?)ex.StatusCode ?? StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsJsonAsync(ex.Message);
+                await context.Response.WriteAsJsonAsync(GetErrorMessage(ex.Message, context.Response.StatusCode));
             }
             catch (Exception ex)
             {
                 _logger.Error(ex, "An exception is captured in {0}", nameof(ExceptionsHandlingMiddleware));
                 context.Response.Headers[HeaderNames.ContentType] = MediaTypeNames.Application.Json;
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsJsonAsync(ex.Message);
+                await context.Response.WriteAsJsonAsync(GetErrorMessage(ex.Message, context.Response.StatusCode));
             }
+        }
+
+        private static string GetErrorMessage(string message, int code)
+        {
+            return @$"{code} {ReasonPhrases.GetReasonPhrase(code)} {message}";
         }
     }
 }
